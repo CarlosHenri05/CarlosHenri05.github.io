bed0bdf6cf27e2e01a2959e5af9a9598c697de4e
Carlos <carloshenrique20051bb@gmail.com>
2025-01-09 04:11:22 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+const dynamicText = document.querySelector(".dynamic-text");
+
+const messages = ["Dev Backend", "Analista Cybersec", "Entusiasta de Programação"];
+let index = 0;
+
+function typeEffect() {
+    let message = messages[index];
+    let charIndex = 0;
+
+    const interval = setInterval(() => {
+        if (charIndex < message.length) {
+            dynamicText.textContent += message[charIndex];
+            charIndex++;
+        } else {
+            clearInterval(interval);
+            setTimeout(() => {
+                deleteEffect();
+            }, 1500);
+        }
+    }, 100);
+}
+
+function deleteEffect() {
+    let message = messages[index];
+    let charIndex = message.length - 1;
+
+    const interval = setInterval(() => {
+        if (charIndex >= 0) {
+            dynamicText.textContent = message.slice(0, charIndex);
+            charIndex--;
+        } else {
+            clearInterval(interval);
+            index = (index + 1) % messages.length;
+            typeEffect();
+        }
+    }, 100);
+}
+
+// Inicia o efeito
+typeEffect();
